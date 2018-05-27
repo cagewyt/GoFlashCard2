3da996e170931654e265f3c0dbbc787c4ce88b34
Yutao <wangytustc@gmail.com>
2018-05-27 12:06:22 -0700

    card list done

--- app/src/main/java/com/cagewyt/goflashcard/CardListActivity.java
+++ app/src/main/java/com/cagewyt/goflashcard/CardListActivity.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.cagewyt.goflashcard.model.FlashCard;
+import com.cagewyt.goflashcard.model.FlashCardSet;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CardListActivity extends AppCompatActivity {
     private String flashCardSetId;
+    private RecyclerView cardListView;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,56 @@ public class CardListActivity extends AppCompatActivity {
                 startActivity(addCardActivity);
             }
         });
+
+        cardListView = (RecyclerView)findViewById(R.id.card_list);
+        cardListView.setHasFixedSize(true);
+        cardListView.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("FlashCardSets").child(flashCardSetId).child("flashCards");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<FlashCard, CardListActivity.FlashCardViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<FlashCard, CardListActivity.FlashCardViewHolder>(
+                        FlashCard.class,
+                        R.layout.flashcard_row,
+                        CardListActivity.FlashCardViewHolder.class,
+                        databaseReference
+                ) {
+                    @Override
+                    protected void populateViewHolder(CardListActivity.FlashCardViewHolder viewHolder, FlashCard model, int position) {
+                        final String flashCardSetKey = getRef(position).getKey().toString();
+
+                        viewHolder.setName(model.getName());
+                        viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+//                                Intent cardListActivity = new Intent(CardSetListActivity.this, CardListActivity.class);
+//                                cardListActivity.putExtra("flashCardSetId", flashCardSetKey);
+//                                startActivity(cardListActivity);
+                            }
+                        });
+                    }
+                };
+
+        cardListView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class FlashCardViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        public FlashCardViewHolder(View itemView)
+        {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setName(String name)
+        {
+            TextView taskName = (TextView)mView.findViewById(R.id.flashCardName);
+            taskName.setText(name);
+        }
+    }
 }
